a5aef8826a19ee1f29b35bf31a76cd9c0166a6da
prashanthreddy01 <prashanthr598@gmail.com>
2023-12-06 12:27:44 +0530

    Adding File

---
+++ src/main/java/com/example/demo/SpidermanApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpidermanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpidermanApplication.class, args);
+		System.out.println("HELLO SPIDY");
+	}
+
+}
